5066f14192c9972516469a9c97d7133b13011452
lsowen <logan@s1network.com>
2015-12-13 10:46:07 -0500

    Re-arrange source code to remove need to use sync

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 	"golang.org/x/net/ipv6"
 	"net"
-	"sync"
 	"time"
 )
 
@@ -45,174 +43,169 @@ type Result struct {
 	Error error
 }
 
-func ping(targetIps []net.IP) (<-chan Result, error) {
-	responseChannel := make(chan Result, len(targetIps))
-	wg := &sync.WaitGroup{}
+func pingIp(targetIp net.IP) (result Result) {
+	isIPv6 := targetIp.To4() == nil
 
-	processor := func(targetIp net.IP) {
-		wg.Add(1)
-		defer wg.Done()
+	network := "ip6:ipv6-icmp"
+	if isIPv6 == false {
+		network = "ip4:icmp"
+	}
 
-		isIPv6 := targetIp.To4() == nil
-
-		network := "ip6:ipv6-icmp"
-		if isIPv6 == false {
-			network = "ip4:icmp"
+	c, err := icmp.ListenPacket(network, "")
+	if err != nil {
+		result = Result{
+			Type:  ERROR,
+			Error: err,
+			Peer:  targetIp,
 		}
 
-		c, err := icmp.ListenPacket(network, "")
+		return
+	}
+	defer c.Close()
+
+	var wm icmp.Message
+	if isIPv6 == false {
+		wm.Type = ipv4.ICMPTypeEcho
+		wm.Code = 0
+		wm.Body = &icmp.Echo{
+			ID:   1,
+			Seq:  1,
+			Data: []byte("ping"),
+		}
+
+	} else {
+		wm.Type = ipv6.ICMPTypeEchoRequest
+		wm.Code = 0
+		wm.Body = &icmp.Echo{
+			ID:   1,
+			Seq:  1,
+			Data: []byte("ping"),
+		}
+	}
+
+	wb, err := wm.Marshal(nil)
+	if err != nil {
+		result = Result{
+			Type:  ERROR,
+			Error: err,
+			Peer:  targetIp,
+		}
+
+		return
+	}
+
+	addr := &net.IPAddr{IP: targetIp}
+
+	_, err = c.WriteTo(wb, addr)
+	if err != nil {
+		result = Result{
+			Type:  ERROR,
+			Error: err,
+			Peer:  targetIp,
+		}
+
+		return
+	}
+
+	rb := make([]byte, 1500)
+
+	timeout := time.Duration(5 * time.Second)
+	c.SetReadDeadline(time.Now().Add(timeout))
+
+	for {
+		n, peer, err := c.ReadFrom(rb)
 		if err != nil {
-			responseChannel <- Result{
-				Type:  ERROR,
-				Error: err,
-				Peer:  targetIp,
-			}
-
-			return
-		}
-		defer c.Close()
-
-		var wm icmp.Message
-		if isIPv6 == false {
-			wm.Type = ipv4.ICMPTypeEcho
-			wm.Code = 0
-			wm.Body = &icmp.Echo{
-				ID:   1,
-				Seq:  1,
-				Data: []byte("ping"),
-			}
-
-		} else {
-			wm.Type = ipv6.ICMPTypeNeighborSolicitation
-			wm.Code = 0
-			wm.Body = &NeighborSolicitation{
-				TargetAddress: targetIp,
-			}
-		}
-
-		wb, err := wm.Marshal(nil)
-		if err != nil {
-			responseChannel <- Result{
-				Type:  ERROR,
-				Error: err,
-				Peer:  targetIp,
-			}
-
-			return
-		}
-
-		addr := &net.IPAddr{IP: targetIp}
-
-		_, err = c.WriteTo(wb, addr)
-		if err != nil {
-			responseChannel <- Result{
-				Type:  ERROR,
-				Error: err,
-				Peer:  targetIp,
-			}
-
-			return
-		}
-
-		rb := make([]byte, 1500)
-
-		timeout := time.Duration(5 * time.Second)
-		c.SetReadDeadline(time.Now().Add(timeout))
-
-		for {
-			n, peer, err := c.ReadFrom(rb)
-			if err != nil {
-				if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
-					responseChannel <- Result{
-						Type: UNREACHABLE,
-						Peer: targetIp,
-					}
-				} else {
-					responseChannel <- Result{
-						Type:  ERROR,
-						Error: err,
-						Peer:  targetIp,
-					}
+			if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
+				result = Result{
+					Type: UNREACHABLE,
+					Peer: targetIp,
 				}
-
 				return
-			}
-
-			proto := ProtocolIPv6ICMP
-			if isIPv6 == false {
-				proto = ProtocolICMP
-			}
-
-			rm, err := icmp.ParseMessage(proto, rb[:n])
-			if err != nil {
-				responseChannel <- Result{
+			} else {
+				result = Result{
 					Type:  ERROR,
 					Error: err,
 					Peer:  targetIp,
 				}
+				return
+			}
+		}
+
+		proto := ProtocolIPv6ICMP
+		if isIPv6 == false {
+			proto = ProtocolICMP
+		}
+
+		rm, err := icmp.ParseMessage(proto, rb[:n])
+
+		if err != nil {
+			result = Result{
+				Type:  ERROR,
+				Error: err,
+				Peer:  targetIp,
+			}
+
+			return
+		}
+		switch rm.Type {
+		case ipv6.ICMPTypeEchoReply:
+			if targetIp.Equal(peer.(*net.IPAddr).IP) {
+				result = Result{
+					Type: REACHABLE,
+					Peer: targetIp,
+				}
 
 				return
 			}
-			switch rm.Type {
-			case ipv6.ICMPTypeNeighborAdvertisement:
-				if targetIp.Equal(peer.(*net.IPAddr).IP) {
-					responseChannel <- Result{
-						Type: REACHABLE,
-						Peer: targetIp,
-					}
+		case ipv6.ICMPTypeDestinationUnreachable:
+			msg := rm.Body.(*icmp.DstUnreach)
+			destination := make(net.IP, net.IPv6len)
+			copy(destination, msg.Data[24:24+net.IPv6len])
 
-					return
+			if targetIp.Equal(destination) {
+				result = Result{
+					Type: UNREACHABLE,
+					Peer: targetIp,
 				}
-			case ipv6.ICMPTypeDestinationUnreachable:
-				msg := rm.Body.(*icmp.DstUnreach)
-				destination := make(net.IP, net.IPv6len)
-				copy(destination, msg.Data[24:24+net.IPv6len])
 
-				if targetIp.Equal(destination) {
-					responseChannel <- Result{
-						Type: UNREACHABLE,
-						Peer: targetIp,
-					}
-
-					return
-				}
-			case ipv4.ICMPTypeEchoReply:
-				if targetIp.Equal(peer.(*net.IPAddr).IP) {
-					responseChannel <- Result{
-						Type: REACHABLE,
-						Peer: targetIp,
-					}
-
-					return
-				}
-			case ipv4.ICMPTypeDestinationUnreachable:
-				msg := rm.Body.(*icmp.DstUnreach)
-				destination := make(net.IP, net.IPv4len)
-				copy(destination, msg.Data[16:16+net.IPv4len])
-				if targetIp.Equal(destination) {
-					responseChannel <- Result{
-						Type: UNREACHABLE,
-						Peer: targetIp,
-					}
-
-					return
-				}
-			default:
-				fmt.Printf("Other packet %+v\n", rm)
+				return
 			}
+		case ipv4.ICMPTypeEchoReply:
+			if targetIp.Equal(peer.(*net.IPAddr).IP) {
+				result = Result{
+					Type: REACHABLE,
+					Peer: targetIp,
+				}
+
+				return
+			}
+		case ipv4.ICMPTypeDestinationUnreachable:
+			msg := rm.Body.(*icmp.DstUnreach)
+			destination := make(net.IP, net.IPv4len)
+			copy(destination, msg.Data[16:16+net.IPv4len])
+			if targetIp.Equal(destination) {
+				result = Result{
+					Type: UNREACHABLE,
+					Peer: targetIp,
+				}
+
+				return
+			}
+		default:
+			//fmt.Printf("Other packet %+v\n", rm)
 		}
 	}
+}
+
+func ping(targetIps []net.IP, responseChannel chan<- Result) {
 
 	for _, targetIp := range targetIps {
-		go processor(targetIp)
+		go func(targetIp net.IP) {
+			responseChannel <- pingIp(targetIp)
+		}(targetIp)
 	}
 
-	go func() {
-		wg.Wait()
-		close(responseChannel)
-	}()
-
-	return responseChannel, nil
+	return
 }
 
 type ipList []net.IP
@@ -224,7 +217,7 @@ func (i *ipList) String() string {
 func (i *ipList) Set(value string) error {
 	ip := net.ParseIP(value)
 	if ip == nil {
-		return errors.New(fmt.Sprintf("%s is not a valid IP", value))
+		return fmt.Errorf("%s is not a valid IP", value)
 	}
 	*i = append(*i, ip)
 	return nil
@@ -240,18 +233,16 @@ func main() {
 	flag.IntVar(&callInterval, "interval", 60, "How often to try fetching new results")
 	flag.Parse()
 
+	responseChannel := make(chan Result, len(targetIPs))
+	go ping(targetIPs, responseChannel)
+	ticker := time.NewTicker(time.Second * time.Duration(callInterval))
 	for {
-		responseChannel, err := ping(targetIPs)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println("before")
-		for r := range responseChannel {
+		select {
+		case r := <-responseChannel:
 			fmt.Printf("Response Message: %+v\n", r)
+		case <-ticker.C:
+			fmt.Printf("Ticker expired\n")
+			go ping(targetIPs, responseChannel)
 		}
-		fmt.Println("after")
-
-		time.Sleep(time.Duration(callInterval) * time.Second)
 	}
 }
